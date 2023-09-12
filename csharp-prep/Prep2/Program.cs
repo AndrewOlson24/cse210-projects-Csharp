eff89385393ee9f5df9ce5bdfe662cb1b4592a14
AndrewOlson24 <andy.w.olson@gmail.com>
2023-09-11 18:34:31 -0600

    9/11/23

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Hello and Welcome to your future...");
+        Console.Write("What is your Grade percentage? ");
+        string answer = Console.ReadLine();
+        int percentage = int.Parse(answer);
+        //a way to make the answer(string) into a number(int)
+
+        string letter = "";
+        
+
+
+        if (percentage>= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";        
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";        
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";        
+        }
+        else
+        {
+            letter = "F";        
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("congrats, you passed!!!");
+        }
+        else
+        {
+            Console.WriteLine("Better Luck Next Time....");
+        }
     }
 }
