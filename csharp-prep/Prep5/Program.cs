197bdd412eb0e6dcad456fac76c6f5b67310566d
AndrewOlson24 <andy.w.olson@gmail.com>
2023-09-12 15:21:07 -0600

    examples of functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,58 @@ using System;
 
 class Program
 {
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+
+        DisplayMessage();
+
+        string playerName = PromptUserName();
+        int playerNumber = PromptUserNumber();
+        int theSquare = SquareNumber(playerNumber);
+
+        DisplayResult(playerName, theSquare);
+
+
+
+
+
+
+        static void DisplayMessage()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("what is your name? ");
+            string name = Console.ReadLine();
+
+            return name;
+            
+        }
+
+        static int PromptUserNumber(){
+
+            Console.Write("What is your favorite number? ");
+            int first = int.Parse(Console.ReadLine());
+
+            return first;
+
+        }
+        static int SquareNumber(int number)
+        {
+            int square = number*number;
+
+            return square;
+        }
+
+        static void DisplayResult(string name, int square)
+        {
+            Console.WriteLine($"{name}, the square of your number is {square}");
+        } 
     }
+
+    
+    
 }
